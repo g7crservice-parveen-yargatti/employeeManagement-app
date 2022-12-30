2fd8ac8654a6488d09a31551295c1c1748e2cd83
g7crservice-parveen-yargatti <parveen.yargatti@g7cr.com>
2022-12-30 13:28:31 +0530

    deployed url (backend)

--- src/config/axiosconfig.ts
+++ src/config/axiosconfig.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const axiosInstance = axios.create({
-    baseURL: 'http://localhost:3000/employeedata',
+    baseURL: 'https://userdataapi-emp-mgt-sys.onrender.com/employeedata',
     headers:{ 'Content-Type': 'application/json',},
     timeout: 5000,
     timeoutErrorMessage: 'Operation timed out'
